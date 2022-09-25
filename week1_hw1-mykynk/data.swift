f0634632f82841320267e92556788d9a51159bfc
Muhammed Yusuf Kaynak <mykynk1@gmail.com>
2022-09-25 17:29:37 +0300

    Project has updated

--- week1_hw1-mykynk/data.swift
+++ week1_hw1-mykynk/data.swift
@@ -7,166 +7,202 @@
 
 import Foundation
 public class SpongeBobData{
-    public struct Shop {
-        var name: String
-        var categoryList: Array<Category>
-    }
-    public struct Category {
-        var name: String
-        var productList: Array<Product>
-    }
-    public struct Product {
-        var name: String
-        var price: Int
-        var review: String
-    }
     
     var shoppingCart = Array<Product>()
     var favoriteList = Array<Product>()
-    public let shopList = [
+    public var shopList = [
         Shop(name: "Krusty Krab", categoryList: [
             Category(name: "Hamburger", productList: [
-                Product(name: "Yengeç Köftesi", price: 80, review: ""),
-                Product(name: "İkili Yengeç Köftesi", price: 100, review: ""),
-                Product(name: "Üçlü Yengeç Köftesi", price: 120, review: ""),
+                Product(name: "Yengeç Köftesi", price: 80, reviews: ["Çok lezzetli!", "Hiç beğenmedim!"]),
+                Product(name: "İkili Yengeç Köftesi", price: 100, reviews: []),
+                Product(name: "Üçlü Yengeç Köftesi", price: 120, reviews: []),
             ]),
             Category(name: "Hamburger Menüleri", productList: [
-                Product(name: "Yengeç Menü", price: 90, review: ""),
-                Product(name: "İkili Yengeç Menü", price: 110, review: ""),
-                Product(name: "Üçlü Yengeç Menü", price: 130, review: ""),
+                Product(name: "Yengeç Menü", price: 90, reviews: []),
+                Product(name: "İkili Yengeç Menü", price: 110, reviews: ["Çok pahalı!", "Bu fiyata çok iyi!"]),
+                Product(name: "Üçlü Yengeç Menü", price: 130, reviews: []),
             ]),
             Category(name: "Yan Lezzetler", productList: [
-                Product(name: "Yengeç Kızartması Küçük", price: 30, review: ""),
-                Product(name: "Yengeç Kızartması Orta", price: 40, review: ""),
-                Product(name: "Yengeç Kızartması Büyük", price: 50, review: ""),
-                Product(name: "Yosun Halkası", price: 50, review: ""),
+                Product(name: "Yengeç Kızartması Küçük", price: 30, reviews: []),
+                Product(name: "Yengeç Kızartması Orta", price: 40, reviews: []),
+                Product(name: "Yengeç Kızartması Büyük", price: 50, reviews: ["Kesinlikle yemelisiniz!", "Hiç beğenmedim!"]),
+                Product(name: "Yosun Halkası", price: 50, reviews: []),
             ]),
             Category(name: "İçecek", productList: [
-                Product(name: "Yengeç Shake", price: 20, review: ""),
-                Product(name: "Denizköpüğü Soda", price: 20, review: ""),
-                Product(name: "Coca - Cola", price: 20, review: ""),
-                Product(name: "Yosun Shake", price: 20, review: ""),
+                Product(name: "Yengeç Shake", price: 20, reviews: []),
+                Product(name: "Denizköpüğü Soda", price: 20, reviews: []),
+                Product(name: "Coca - Cola", price: 20, reviews: []),
+                Product(name: "Yosun Shake", price: 20, reviews: []),
             ]),
         ]),
         
         Shop(name: "Chum Bucket", categoryList: [
             Category(name: "Chum", productList: [
-                Product(name: "Sosisli Chum", price: 50, review: ""),
-                Product(name: "Patatesli Chum", price: 40, review: ""),
-                Product(name: "Peynirli Chum", price: 45, review: ""),
-                Product(name: "Acılı Chum", price: 50, review: ""),
+                Product(name: "Sosisli Chum", price: 50, reviews: []),
+                Product(name: "Patatesli Chum", price: 40, reviews: []),
+                Product(name: "Peynirli Chum", price: 45, reviews: []),
+                Product(name: "Acılı Chum", price: 50, reviews: []),
             ]),
             Category(name: "Yan Lezzetler", productList: [
-                Product(name: "Chum Patates Kızartması - Küçük", price: 30, review: ""),
-                Product(name: "Chum Patates Kızartması - Orta", price: 40, review: ""),
-                Product(name: "Chum Patates Kızartması - Büyük", price: 50, review: ""),
+                Product(name: "Chum Patates Kızartması - Küçük", price: 30, reviews: []),
+                Product(name: "Chum Patates Kızartması - Orta", price: 40, reviews: []),
+                Product(name: "Chum Patates Kızartması - Büyük", price: 50, reviews: []),
             ]),
             Category(name: "İçecek", productList: [
-                Product(name: "Chum Shake", price: 20, review: ""),
-                Product(name: "Coca - Cola", price: 20, review: ""),
+                Product(name: "Chum Shake", price: 20, reviews: []),
+                Product(name: "Coca - Cola", price: 20, reviews: []),
             ]),
         ]),
         
-        Shop(name: "SANDY", categoryList: [
+        Shop(name: "Sandy", categoryList: [
             Category(name: "Giyim", productList: [
-                Product(name: "Astronot Kıyafetleri", price: 800, review: ""),
-                Product(name: "Kaban", price: 600, review: ""),
-                Product(name: "Mayo", price: 200, review: ""),
-                Product(name: "Bikini", price: 200, review: ""),
+                Product(name: "Astronot Kıyafetleri", price: 800, reviews: []),
+                Product(name: "Kaban", price: 600, reviews: []),
+                Product(name: "Mayo", price: 200, reviews: []),
+                Product(name: "Bikini", price: 200, reviews: []),
             ]),
             Category(name: "Spor", productList: [
-                Product(name: "Koşu Diski", price: 100, review: ""),
-                Product(name: "Karate Eldiveni", price: 200, review: ""),
-                Product(name: "Karate Başlığı", price: 150, review: ""),
-                Product(name: "Mat", price: 450, review: ""),
+                Product(name: "Koşu Diski", price: 100, reviews: []),
+                Product(name: "Karate Eldiveni", price: 200, reviews: []),
+                Product(name: "Karate Başlığı", price: 150, reviews: []),
+                Product(name: "Mat", price: 450, reviews: []),
             ]),
             Category(name: "Ofis", productList: [
-                Product(name: "Mikroskop", price: 1200, review: ""),
-                Product(name: "Kalem", price: 30, review: ""),
-                Product(name: "Defter", price: 20, review: ""),
+                Product(name: "Mikroskop", price: 1200, reviews: []),
+                Product(name: "Kalem", price: 30, reviews: []),
+                Product(name: "Defter", price: 20, reviews: []),
             ]),
             Category(name: "Kitaplar ve Hobiler", productList: [
-                Product(name: "Deniz Yaşamı Hakkında Her Şey Kitap ", price: 50, review: ""),
-                Product(name: "Puzzle", price: 170, review: ""),
-                Product(name: "Kış Uykusu Hakkında Bilinmesi Gerekenler", price: 55, review: ""),
-                Product(name: "Uzay", price: 55, review: ""),
-                Product(name: "Uzay Mekikleri", price: 65, review: ""),
-                Product(name: "Teksas Tarihi", price: 76, review: ""),
+                Product(name: "Deniz Yaşamı Hakkında Her Şey Kitap ", price: 50, reviews: []),
+                Product(name: "Puzzle", price: 170, reviews: []),
+                Product(name: "Kış Uykusu Hakkında Bilinmesi Gerekenler", price: 55, reviews: []),
+                Product(name: "Uzay", price: 55, reviews: []),
+                Product(name: "Uzay Mekikleri", price: 65, reviews: []),
+                Product(name: "Teksas Tarihi", price: 76, reviews: []),
                 
             ]),
             Category(name: "Ev Eşyaları", productList: [
-                Product(name: "Hava Temizleme Filtresi", price: 980, review: ""),
-                Product(name: "Su boşaltım pompası", price: 1300, review: ""),
-                Product(name: "Robot", price: 5000, review: ""),
-                Product(name: "Fındık Kıracağı", price: 70, review: ""),
-                Product(name: "Pire Tasması", price: 80, review: ""),
+                Product(name: "Hava Temizleme Filtresi", price: 980, reviews: []),
+                Product(name: "Su boşaltım pompası", price: 1300, reviews: []),
+                Product(name: "Robot", price: 5000, reviews: []),
+                Product(name: "Fındık Kıracağı", price: 70, reviews: []),
+                Product(name: "Pire Tasması", price: 80, reviews: []),
             ]),
         ]),
         
         Shop(name: "Süngerbob Karepantalon", categoryList: [
             Category(name: "Giyim", productList: [
-                Product(name: "Pantolon", price: 400, review: ""),
-                Product(name: "Şort", price: 250, review: ""),
-                Product(name: "İç Çamaşırı", price: 170, review: ""),
-                Product(name: "Kravat", price: 200, review: ""),
-                Product(name: "Çorap", price: 35, review: ""),
-                Product(name: "Ayakkabı", price: 900, review: ""),
-                Product(name: "Krusty Krab Şapka", price: 150, review: ""),
-                Product(name: "Pijama Takımı", price: 210, review: ""),
-                Product(name: "Gıcırdayan Çizmeler", price: 780, review: ""),
+                Product(name: "Pantolon", price: 400, reviews: []),
+                Product(name: "Şort", price: 250, reviews: []),
+                Product(name: "İç Çamaşırı", price: 170, reviews: []),
+                Product(name: "Kravat", price: 200, reviews: []),
+                Product(name: "Çorap", price: 35, reviews: []),
+                Product(name: "Ayakkabı", price: 900, reviews: []),
+                Product(name: "Krusty Krab Şapka", price: 150, reviews: []),
+                Product(name: "Pijama Takımı", price: 210, reviews: []),
+                Product(name: "Gıcırdayan Çizmeler", price: 780, reviews: []),
             ]),
             Category(name: "Evcil Hayvan Bakımı", productList: [
-                Product(name: "Salyangoz (Kedi) Yemi (10 Kg)", price: 1500, review: ""),
-                Product(name: "Kurt (Köpek) Yemi (10 Kg)", price: 2100, review: ""),
-                Product(name: "Salyangoz (Kedi) Kumu (20L)", price: 190, review: ""),
-                Product(name: "Oyuncak", price: 80, review: ""),
-                Product(name: "Ödül Mamaları", price: 15, review: ""),
+                Product(name: "Salyangoz (Kedi) Yemi (10 Kg)", price: 1500, reviews: []),
+                Product(name: "Kurt (Köpek) Yemi (10 Kg)", price: 2100, reviews: []),
+                Product(name: "Salyangoz (Kedi) Kumu (20L)", price: 190, reviews: []),
+                Product(name: "Oyuncak", price: 80, reviews: []),
+                Product(name: "Ödül Mamaları", price: 15, reviews: []),
             ]),
             Category(name: "Kitaplar ve Hobiler", productList: [
-                Product(name: "Ağ", price: 320, review: ""),
-                Product(name: "Olta", price: 400, review: ""),
-                Product(name: "Deniz Anaları Hakkında Her Şey", price: 65, review: ""),
-                Product(name: "Krusty Krab Tarihi", price: 55, review: ""),
-                Product(name: "Deniz Kızı Adam ve Miye Oğlan Çizgi Romanı", price: 70, review: ""),
-                Product(name: "Deniz Kızı Adam Figür", price: 160, review: ""),
-                Product(name: "Midye Oğlan Figür", price: 160, review: ""),
-                Product(name: "Korku Kitabı", price: 35, review: ""),
-                Product(name: "Uçan Hollandalı Hayatı", price: 65, review: ""),
-                Product(name: "Uçan Hollandalı Hikayeleri", price: 65, review: ""),
-                Product(name: "Yılanlar ve Merdivenler Oyunu", price: 210, review: ""),
-                Product(name: "Baloncuk Kutusu", price: 50, review: ""),
+                Product(name: "Ağ", price: 320, reviews: []),
+                Product(name: "Olta", price: 400, reviews: []),
+                Product(name: "Deniz Anaları Hakkında Her Şey", price: 65, reviews: []),
+                Product(name: "Krusty Krab Tarihi", price: 55, reviews: []),
+                Product(name: "Deniz Kızı Adam ve Miye Oğlan Çizgi Romanı", price: 70, reviews: []),
+                Product(name: "Deniz Kızı Adam Figür", price: 160, reviews: []),
+                Product(name: "Midye Oğlan Figür", price: 160, reviews: []),
+                Product(name: "Korku Kitabı", price: 35, reviews: []),
+                Product(name: "Uçan Hollandalı Hayatı", price: 65, reviews: []),
+                Product(name: "Uçan Hollandalı Hikayeleri", price: 65, reviews: []),
+                Product(name: "Yılanlar ve Merdivenler Oyunu", price: 210, reviews: []),
+                Product(name: "Baloncuk Kutusu", price: 50, reviews: []),
             ]),
             Category(name: "Mutfak Gereçleri", productList: [
-                Product(name: "Spatula", price: 350, review: ""),
-                Product(name: "Izgara", price: 1700, review: ""),
-                Product(name: "Kızartma Izgarası", price: 1600, review: ""),
-                Product(name: "Süzgeç", price: 150, review: ""),
-                Product(name: "Bıçak", price: 230, review: ""),
+                Product(name: "Spatula", price: 350, reviews: []),
+                Product(name: "Izgara", price: 1700, reviews: []),
+                Product(name: "Kızartma Izgarası", price: 1600, reviews: []),
+                Product(name: "Süzgeç", price: 150, reviews: []),
+                Product(name: "Bıçak", price: 230, reviews: []),
             ]),
         ]),
         Shop(name: "Patrick", categoryList: [
             Category(name: "Ev Eşyaları", productList: [
-                Product(name: "Televizyon", price: 2300, review: ""),
-                Product(name: "Uyku Başlığı", price: 90, review: ""),
-                Product(name: "Koltuk", price: 2000, review: ""),
+                Product(name: "Televizyon", price: 2300, reviews: []),
+                Product(name: "Uyku Başlığı", price: 90, reviews: []),
+                Product(name: "Koltuk", price: 2000, reviews: []),
             ]),
         ]),
         Shop(name: "Squidward", categoryList: [
             Category(name: "Hobi", productList: [
-                Product(name: "Klarnet", price: 2400, review: ""),
-                Product(name: "Boş Tuval", price: 150, review: ""),
-                Product(name: "Boya", price: 100, review: ""),
-                Product(name: "Fırçalar", price: 65, review: ""),
-                Product(name: "Heykel ", price: 235, review: ""),
-                Product(name: "Şövale", price: 1500, review: ""),
+                Product(name: "Klarnet", price: 2400, reviews: []),
+                Product(name: "Boş Tuval", price: 150, reviews: []),
+                Product(name: "Boya", price: 100, reviews: []),
+                Product(name: "Fırçalar", price: 65, reviews: []),
+                Product(name: "Heykel ", price: 235, reviews: []),
+                Product(name: "Şövale", price: 1500, reviews: []),
             ]),
             Category(name: "Giyim", productList: [
-                Product(name: "Tişört", price: 120, review: ""),
-                Product(name: "Frak", price: 2650, review: ""),
-                Product(name: "Gözlük", price: 500, review: ""),
+                Product(name: "Tişört", price: 120, reviews: []),
+                Product(name: "Frak", price: 2650, reviews: []),
+                Product(name: "Gözlük", price: 500, reviews: []),
             ]),
         ]),
     ]
 }
 
+public class Shop {
+    var name: String
+    var categoryList: Array<Category>
+    
+    init (name: String, categoryList: Array<Category>) {
+        self.name = name
+        self.categoryList = categoryList
+    }
+}
 
+
+public class Category {
+    var name: String
+    var productList: Array<Product>
+    
+    init(name: String, productList: Array<Product>) {
+        self.name = name
+        self.productList =  productList
+    }
+}
+
+public class Product {
+    var name: String
+    var price: Int
+    var reviews: Array<String>
+    
+    init(name: String, price: Int, reviews: Array<String>) {
+        self.name = name
+        self.price = price
+        self.reviews = reviews
+    }
+}
+
+public class Person {
+    var name: String
+    var surName: String
+    var gender: String
+    var age: String
+    var job: String
+    var favoriteProduct: Array<Product>
+    
+    init(name: String, surName: String, gender: String, age: String, job: String, favoriteProduct: Array<Product> = []){
+        self.name = name
+        self.surName = surName
+        self.gender = gender
+        self.age = age
+        self.job = job
+        self.favoriteProduct = favoriteProduct
+    }
+}
--- week1_hw1-mykynk/main.swift
+++ week1_hw1-mykynk/main.swift
@@ -6,131 +6,185 @@
 //
 
 import Foundation
-let data = SpongeBobData()
+var data = SpongeBobData()
 
-//var person = (name: "", surname: "", gender: "", age: "", job: "")
-//print("--- KAYDOL ---")
-//
-//print("Ad :  ")
-//person.name  = readLine(strippingNewline: true) ?? ""
-//print()
-//
-//print("Soyad :  ")
-//person.surname = readLine() ?? ""
-//print()
-//
-//print("Cinsiyet: ")
-//person.gender = readLine() ?? ""
-//print()
-//
-//print("Yaş: ")
-//person.age = readLine() ?? ""
-//print()
-//
-//print("Meslek : ")
-//person.job = readLine() ?? ""
-//
-//print()
-//print("Hoşgeldiniz \(person.name) \(person.surname) ")
-//print("Cinsiyet : \(person.gender) | Yaş : \(person.age) | Meslek : \(person.job) ")
-//print()
-//
+print("--- KAYDOL ---")
+
+print("Adınız:  ")
+let personName  = readLine(strippingNewline: true) ?? ""
+print()
+
+print("Soyadınız:  ")
+let personSurname = readLine() ?? ""
+print()
+
+print("Cinsiyetiniz: ")
+let personGender = readLine() ?? ""
+print()
+
+print("Yaşınız: ")
+let personAge = readLine() ?? ""
+print()
+
+print("Mesleğiniz: ")
+let personJob = readLine() ?? ""
+let person = Person(name: personName, surName: personSurname, gender: personGender, age: personAge, job: personJob)
+
+print()
+print("Süngerbob Pazaryerine hoşgeldiniz")
+print("Merhaba \(person.name) \(person.surName) ")
+print("Cinsiyetiniz : \(person.gender) | Yaşınız : \(person.age) | Mesleğiniz : \(person.job) ")
+print()
+
 while true {
     //MARK: - Mağaza Listesi
+    let shopList = data.shopList
     print("-- Mağazalar --")
     var i = 0
-    for shop in data.shopList{
+    for shop in shopList{
         i = i + 1
         print( String(i) + " - " + shop.name)
     }
+    print()
+    print("9 - Favori Ürünlerim")
     print("0 - Çıkış")
     print()
     print("Lütfen seçmek istediğiniz mağazanın numarasını giriniz: ")
+
     let selectedShopIndex = Int(readLine() ?? "1")
+    if selectedShopIndex == 0 {
+        break
+    }else if(selectedShopIndex == 9){
+        print()
+        if person.favoriteProduct.count == 0 {
+            print("Favori ürününüz bulunmamaktadır.")
+            
+        }else {
+            print("Favori ürünleriniz: ")
+            for favorite in person.favoriteProduct {
+                print("\(favorite.name) - \(favorite.price) TL")
+            }
+        }
+        print()
+       
+        continue
+    }
     print()
     
 outerLoop: while true {
     //MARK: - Kategori Listesi
+    let categoryList = shopList[(selectedShopIndex ?? 1 ) - 1].categoryList
     print("-- Kategoriler --")
     var j = 0
-    for category in data.shopList[(selectedShopIndex ?? 1 ) - 1].categoryList{
+    for category in categoryList{
         j = j + 1
         print( String(j) + " - " + category.name)
     }
-    print("\(j+1) - Ana Menü")
     print()
+    print("0 - Ana Menü")
+    print()
+    
+    
     
     print("Lütfen seçmek istediğiniz kategorinin numarasını giriniz: ")
     let selectedCategoryIndex = Int(readLine() ?? "1")
+    showShoppingCart()
     print()
+    if selectedCategoryIndex == 0 {
+        break outerLoop
+    }
     
     //MARK: - Seçilen kategori
-    let selectedCategory = data.shopList[(selectedShopIndex ?? 1 ) - 1].categoryList[(selectedCategoryIndex ?? 1 ) - 1 ]
-    print("-- \(selectedCategory.name) -- " )
+    let categoryName = categoryList[(selectedCategoryIndex ?? 1 ) - 1 ].name
+    print("-- " + categoryName + " -- " )
     
     while true {
         //MARK: - Ürün listesi
+        let productList = categoryList[(selectedCategoryIndex ?? 1 ) - 1 ].productList
         var k = 0
-        for product in selectedCategory.productList{
+        for product in productList{
             k = k + 1
-            print(String(k) + " - " + product.name + " - " + String(product.price))
+            print(String(k) + " - " + product.name + " - " + String(product.price) + " TL")
         }
+        print()
+        print("\(k+1) - Kategori Menüsü")
         print()
         print("Lütfen seçmek istediğiniz ürünün numarasını giriniz: ")
         let selectedProductIndex = Int(readLine() ?? "1")
+        print()
+        if selectedProductIndex == (k+1) {
+            break
+        }
         print()
         
         //MARK: - Ürün detay listesi
         print("1 - Ürünü Sepete Ekle")
         print("2 - Ürün yorumlarını görüntüle")
         print("3 - Ürüne yorum ekle")
+        print("4 - Ürünü favoriye ekle")
+        print()
         print("0 - Ana Menü")
         
         print()
         print("Lütfen yapmak istediğiniz işlemin numarasını giriniz: ")
         let selectedDetailIndex = Int(readLine() ?? "1")
-        var selectedProduct = selectedCategory.productList[(selectedDetailIndex ?? 1 ) - 1]
+        
         switch selectedDetailIndex {
         case 0:
             break outerLoop
         case 1:
+            let selectedProduct = productList[(selectedProductIndex ?? 1 ) - 1]
+
             data.shoppingCart.append(selectedProduct)
             break
         case 2:
-            print(selectedProduct.review)
+            print()
+            
+            let selectedProduct = productList[(selectedProductIndex ?? 1 ) - 1]
+            if selectedProduct.reviews.count == 0 {
+                print("Henüz yorum yapılmadı!")
+            }else {
+                for review in selectedProduct.reviews {
+                    print("Yorum : " +  review)
+                }
+            }
+           
+            print()
             break
         case 3:
-            break
-            
+            let selectedProduct = productList[(selectedProductIndex ?? 1 ) - 1]
+
+            print("Lütfen yorumunuzu giriniz : ")
+            let review = readLine() ?? ""
+            selectedProduct.reviews.append(review)
+        case 4:
+            let selectedProduct = productList[(selectedProductIndex ?? 1 ) - 1]
+
+            person.favoriteProduct.append(selectedProduct)
         default:
             break
             
             
         }
+        showShoppingCart()
+
     }
 }
 }
 
-
-
-
-
-/*
- kayıt olmamızı istemektedir - isim, cinsiyet, yaş, ve meslek alanı seçimleri yapmamızı istemektedir - statik liste olabilir
- -- Ana Ekran
- Kayıt olurken girilen bilgiler listelenir
- Ürün seçimi yapılır
- -- Detay Ekranı
- Satıcı bilgisi, ürün puanı ve yorumları
- Ürünler favoriye eklenebilir - Favori ürünler profilde listelenir
- Ürün satın almak isteniyorsa sepete eklenir
- Ödeme yöntemi olacak - yoksa eklemeye yönlenecek
- Promosyon kodu ekleme özelliği
- 
- -- Listeler
- Ürün Kategori listesi
- Ürün  listesi
- Ürünün yorum listesi
- Favori ürün listesi
- 
- */
+func showShoppingCart(){
+    
+    if data.shoppingCart.count != 0 {
+        print()
+        print("Sepetinizdeki ürünler : ")
+        
+        var totalPrice = 0
+        for product in data.shoppingCart {
+            totalPrice = totalPrice + product.price
+            print("\(product.name) - \(product.price) TL")
+        }
+        print("Sepet toplamı : \(totalPrice) TL")
+        
+        print()
+    }
+}
